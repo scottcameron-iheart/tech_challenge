5ba8f982366f8a88528b2771f456d547ea15c666
Henry Kellam <henry.a.kellam@gmail.com>
2022-03-22 13:49:00 -0400

    duplicate funtion declaration fixed

--- react-app/src/api/SongListApi.ts
+++ react-app/src/api/SongListApi.ts
@@ -2,19 +2,12 @@ import axios, { AxiosInstance } from 'axios';
 import Song from '../interface/Song'
 
 export default class SongListApi {
-    private axiosInstance: AxiosInstance
-
-    constructor() {
-        this.axiosInstance = axios.create({
-            baseURL: 'http://localhost:3000',
-            withCredentials: true,
-        })
-    }
-
-    public async getSongData(): Promise<Song[]> {
-
+    async getSongData(): Promise<Song[]> {
+        const url = 'localhost:3000/songs'
         try {
-            const response = await this.axiosInstance.get('/songs');
+            const response = await axios.get(url, {
+                withCredentials: true,
+            });
             const data = JSON.parse(response.data);
             return new Promise(resolve => resolve(data));
         } catch (err) {
