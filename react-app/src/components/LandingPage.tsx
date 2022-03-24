d8bc2ccf3ba1dbca459b3bf265b9cd67cbfc7097
Henry Kellam <henry.a.kellam@gmail.com>
2022-03-24 14:11:36 -0400

    added content to landing page; styled landing

--- react-app/src/components/LandingPage.tsx
+++ react-app/src/components/LandingPage.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import logo from '../logo.svg';
 import { Link } from 'react-router-dom';
+import iHeart from '../IHeartMedia_logo.svg'
+import aws from '../Amazon_Web_Services_Logo.svg'
 
 import './LandingPage.css'
 
 function LandingPage() {
     return (
-        <header className="Landing">
-            <img src={logo} className="logo" alt="logo" />
+        <div className="Landing">
+            <div className='intro'>
+                <p>Hello,<img src={iHeart} className="iheart-logo" alt="i heart media logo" /> team</p>
+                <p>Thank you for the opportunity to complete this coding challenge. I hope that I fufilled the requirments of the assignment
+                    up to your team's standards. Looking forward to hearing from you after you have assesed my work.
+                </p>
+                <p> All the best, <br />-Henry</p>
+            </div>
             <Link to="/songs">Go To Song Page</Link>
-        </header>
+            <div className="logo-container">
+                <h2>Made with: </h2>
+                <img src={logo} className="react-logo" alt="react logo" />
+                <h1>+</h1>
+                <img src={aws} className="aws-logo" alt="aws logo" />
+            </div>
+        </div>
     )
 }
 
